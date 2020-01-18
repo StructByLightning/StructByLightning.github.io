595db92bcf7693b182b5f946584e912a9abbe3d2
resheet <resheetschultz@gmail.com>
2020-01-18 11:30:42 -0800

    .

--- portfolio/src/views/pages/portfolio/portfolio.jsx
+++ portfolio/src/views/pages/portfolio/portfolio.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Home from "./sections/home/home.jsx";
 import './portfolio.scss';
 import About from "./sections/about/about.jsx";
-
+import Contact from "./sections/contact/contact.jsx";
+import Projects from "./sections/projects/projects.jsx";
 
 export default class Portfolio extends React.Component {
   render() {
@@ -10,13 +11,9 @@ export default class Portfolio extends React.Component {
       <div className="portfolio">
         <Home />
         <About />
+        <Projects />
+        <Contact />
       </div>
     );
   }
 }
-
-
-//
-//import Contact from "./sections/contact/contact.jsx";
-
-//import Projects from "./sections/projects/projects.jsx";
