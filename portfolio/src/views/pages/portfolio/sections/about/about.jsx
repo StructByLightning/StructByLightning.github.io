d9b996385bb835c1d169454369c26bfc4276c7f4
resheet <resheetschultz@gmail.com>
2020-01-18 16:52:26 -0800

    move ids

--- portfolio/src/views/pages/portfolio/sections/about/about.jsx
+++ portfolio/src/views/pages/portfolio/sections/about/about.jsx
@@ -5,8 +5,8 @@ import LineHeader from "components/lineHeader/lineHeader.jsx";
 export default class About extends React.Component {
   render() {
     return (
-      <section className="about">
-        <LineHeader label="About" id="section-about" />
+      <section className="about" id="section-about">
+        <LineHeader label="About" />
       </section>
     );
   }
--- portfolio/src/views/pages/portfolio/sections/contact/contact.jsx
+++ portfolio/src/views/pages/portfolio/sections/contact/contact.jsx
@@ -5,8 +5,8 @@ import LineHeader from "components/lineHeader/lineHeader.jsx";
 export default class Contact extends React.Component {
   render() {
     return (
-      <section className="contact">
-        <LineHeader label="Contact" id="section-contact" />
+      <section className="contact" id="section-contact" >
+        <LineHeader label="Contact" />
       </section>
     );
   }
