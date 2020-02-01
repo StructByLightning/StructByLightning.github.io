9af73c0743f4b1c0996d0a8a14ddd3389a255381
resheet <resheetschultz@gmail.com>
2020-01-31 16:21:05 -0800

    contact section

--- portfolio/src/views/pages/portfolio/sections/contact/contact.jsx
+++ portfolio/src/views/pages/portfolio/sections/contact/contact.jsx
@@ -7,6 +7,20 @@ export default class Contact extends React.Component {
     return (
       <section className="contact" id="section-contact" >
         <LineHeader label="Contact" />
+        <div class="content">
+          <div class="email-wrapper">
+            <label for="sender-email">Your email:</label>
+            <input class="input" id="sender-email" />
+          </div>
+          <div class="message-wrapper">
+            <label for="message">Message:</label>
+            <textarea class="input" id="message" />
+          </div>
+          <div class="button-wrapper">
+            <button class="button" onClick={() => { console.log("send an email!") }}>Send</button>
+          </div>
+
+        </div>
       </section>
     );
   }
